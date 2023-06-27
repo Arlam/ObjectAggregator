8d47991ccbc49d72ba766fbc0d8196483ba72093
Andrii Arlamovskyi <andrii.arlamovskyi@vitech.com.ua>
2023-06-27 20:24:46 +0300

    use nunit

--- test/AggregatorTest/UnitTest1.cs
+++ test/AggregatorTest/UnitTest1.cs
@@ -1,24 +1,17 @@
-using Xunit;
-using System.IO;
+using NUnit.Framework;
 
 namespace AggregatorTest;
 
-public class UnitTest1
+public class Tests
 {
-    [Fact]
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
     public void Test1()
     {
-
-        System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo("./test-cases");
-        System.Console.WriteLine("===========================");
-        FileInfo[] files = dir.GetFiles("*.json", System.IO.SearchOption.AllDirectories);
-        System.Console.WriteLine(files);
-        foreach(FileInfo file in files) {
-            System.Console.WriteLine(file);
-        }
-
-        System.Console.WriteLine("===========================");
-        // Assert.Pass();
-        // throw new NotImplementedException("Not fully implemented.");
+        Assert.Pass();
     }
 }
